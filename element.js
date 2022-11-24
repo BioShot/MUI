7df92f999ed46a52086efc1b35dbb77a153ef8cc
Levi <86807700+BioShot@users.noreply.github.com>
2022-11-24 18:18:12 -0500

    Create element.js

---
+++ element.js
@@ -0,0 +1,18 @@
+//Made By BioShot\\
+//this is e:
+    //<button event.click="alert('hi');">Click Me!</button>
+!(function (l, e){
+    t=this;
+    err=(function (b){return console.error(b);})
+    err("test");
+    if(!e && !l){t.r=("no element or callback madebylevia"); err(t.r);return t.error;};
+    !(function (c, e){
+    })(!(function(a){
+        return a;
+    }),e);
+
+
+
+})(!(function (r){
+    return r;
+}));
